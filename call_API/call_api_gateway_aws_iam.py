a8c3e97fc2a6306de86039d6904a75c7f7476bb9
Stavarache, Andrei (IT INF) EXTERNAL <andrei.stavarache.ext@boehringer-ingelheim.com>
2025-10-01 22:19:14 +0300

    refactor

--- call_API/call_api_gateway_aws_iam.py
+++ call_API/call_api_gateway_aws_iam.py
@@ -4,17 +4,15 @@ import hmac
 import requests
 import os
 
-
 HOST = "api.dev-apvm.awsdev.boehringer.com"
 PATH = "/db/accounts_to_close"
-METHOD = "GET"
+METHOD = "POST"  # Change to POST if required
 REGION = "eu-west-1"
 
 # AWS access keys
 access_key = os.environ['AWS_ACCESS_KEY_ID']  
 secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
 session_token = os.environ['AWS_SESSION_TOKEN']
-
 
 # Request parameters
 method = METHOD
@@ -28,12 +26,15 @@ t = datetime.datetime.utcnow()
 amzdate = t.strftime('%Y%m%dT%H%M%SZ')
 datestamp = t.strftime('%Y%m%d') 
 
+# Payload (if required)
+payload = '{"body":{"skip_data_gathering":false,"skip_suspending":false,"skip_closing":true,"DryRun":true}}'
+payload_hash = hashlib.sha256(payload.encode('utf-8')).hexdigest()
+
 # Create the canonical request
 canonical_uri = endpoint
-canonical_querystring = ''
+canonical_querystring = ''  # Add query params if required
 canonical_headers = 'host:' + host + '\n'
 signed_headers = 'host'
-payload_hash = hashlib.sha256(''.encode('utf-8')).hexdigest()
 canonical_request = (method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n'
                      + canonical_headers + '\n' + signed_headers + '\n' + payload_hash)
 
@@ -62,12 +63,15 @@ authorization_header = (algorithm + ' ' + 'Credential=' + access_key + '/' + cre
                         'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature)
 
 # Make the request
-headers = {'Host': host,
-           'x-amz-date': amzdate,
-           'x-amz-security-token': session_token,
-           'Authorization': authorization_header}
+headers = {
+    'Host': host,
+    'x-amz-date': amzdate,
+    'x-amz-security-token': session_token,
+    'Authorization': authorization_header,
+    'Content-Type': 'application/json'  # Add if required
+}
 request_url = 'https://' + host + canonical_uri
-response = requests.get(request_url, headers=headers, timeout=5, verify=False)
+response = requests.post(request_url, headers=headers, data=payload, timeout=5, verify=False)  # Change to POST if required
 response.raise_for_status()
 
 print(response.text)
