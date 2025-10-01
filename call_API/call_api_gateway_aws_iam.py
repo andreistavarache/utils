c6d3519ce884e50b7fa736fa14ee677cf3328bec
Stavarache, Andrei (IT INF) EXTERNAL <andrei.stavarache.ext@boehringer-ingelheim.com>
2025-10-01 22:02:28 +0300

    da

--- call_API/call_api_gateway_aws_iam.py
+++ call_API/call_api_gateway_aws_iam.py
@@ -1,25 +1,73 @@
-import boto3
+import datetime
+import hashlib
+import hmac
 import requests
-from botocore.auth import SigV4Auth
-from botocore.awsrequest import AWSRequest
+import os
 
-# AWS Credentials
-access_key = "ssss"
-secret_key = "ssss"
-region = "eu-west-1"
-service = "execute-api"
 
-# API Endpoint
-url = "https://api.dev-apvm.awsdev.boehringer.com/db/accounts_to_close"
-headers = {
-    "Content-Type": "application/json",
-    "x-amz-date": "20251001T103802Z"
-}
+HOST = "api.dev-apvm.awsdev.boehringer.com"
+PATH = "/db/accounts_to_close"
+METHOD = "GET"
+REGION = "eu-west-1"
 
-# Create AWSRequest
-request = AWSRequest(method="GET", url=url, headers=headers)
-SigV4Auth(boto3.Session().get_credentials(), service, region).add_auth(request)
+# AWS access keys
+access_key = os.environ['AWS_ACCESS_KEY_ID']  
+secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
+session_token = os.environ['AWS_SESSION_TOKEN']
 
-# Send the request with SSL verification disabled
-response = requests.get(url, headers=dict(request.headers), verify=False)
-print(response.status_code, response.text)
+
+# Request parameters
+method = METHOD
+service = 'execute-api'
+host = HOST
+region = REGION
+endpoint = PATH
+
+# Create a datetime object for signing
+t = datetime.datetime.utcnow()
+amzdate = t.strftime('%Y%m%dT%H%M%SZ')
+datestamp = t.strftime('%Y%m%d') 
+
+# Create the canonical request
+canonical_uri = endpoint
+canonical_querystring = ''
+canonical_headers = 'host:' + host + '\n'
+signed_headers = 'host'
+payload_hash = hashlib.sha256(''.encode('utf-8')).hexdigest()
+canonical_request = (method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n'
+                     + canonical_headers + '\n' + signed_headers + '\n' + payload_hash)
+
+# Create the string to sign
+algorithm = 'AWS4-HMAC-SHA256'
+credential_scope = datestamp + '/' + region + '/' + service + '/' + 'aws4_request'
+string_to_sign = (algorithm + '\n' +  amzdate + '\n' +  credential_scope + '\n' +  
+                  hashlib.sha256(canonical_request.encode('utf-8')).hexdigest())
+
+def sign(key, msg):
+    return hmac.new(key, msg.encode("utf-8"), hashlib.sha256).digest()
+
+def getSignatureKey(key, dateStamp, regionName, serviceName):
+    kDate = sign(("AWS4" + key).encode("utf-8"), dateStamp)
+    kRegion = sign(kDate, regionName)
+    kService = sign(kRegion, serviceName)
+    kSigning = sign(kService, "aws4_request")
+    return kSigning
+
+# Sign the string    
+signing_key = getSignatureKey(secret_key, datestamp, region, service)
+signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
+
+# Add signing information to the request
+authorization_header = (algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' +  
+                        'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature)
+
+# Make the request
+headers = {'Host': host,
+           'x-amz-date': amzdate,
+           'x-amz-security-token': session_token,
+           'Authorization': authorization_header}
+request_url = 'https://' + host + canonical_uri
+response = requests.get(request_url, headers=headers, timeout=5, verify=False)
+response.raise_for_status()
+
+print(response.text)
